1a36f79bd416a961cf4765bb9b5528ee396973c4
summenurfatema <shadrinmoni15@gmail.com>
2024-12-04 18:46:13 +0600

    code updated v5

--- server-side/src/app/modules/nft/nft.service.js
+++ server-side/src/app/modules/nft/nft.service.js
@@ -5,14 +5,14 @@ import { NFT } from "./nft.model.js";
 
 //-----------get nft by id
 export const getNftByIdService = async (id) => {
-    const nftById = await NFT.find({ _id : id })
+    const nftById = await NFT.findOne({ _id : id })
       .sort({ createdAt: -1 });
     return nftById;
   };
 
 //-----------get all nft 
 export const getAllNftService = async () => {
-    const allNft = await NFT.findOne({})
+    const allNft = await NFT.find({})
       .sort({ createdAt: -1 });
     return allNft;
   };
